dc956fc425c6814ee35591807e91abdfbdb172ea
Michael Consoli <michael@cartwheelgames.com>
2017-10-22 00:28:16 -0400

    Basic node structure with dialog and such

--- Assets/Source/Tree/TreeNodeSO.cs
+++ Assets/Source/Tree/TreeNodeSO.cs
@@ -5,4 +5,23 @@ using UnityEngine;
 public class TreeNodeSO : ScriptableObject
 {
     public string data = "";
+	public Comment[] comments = new Comment[0];
+	public DialogOption[] options = new DialogOption[0];
+
+	[System.Serializable]
+	public class Comment
+	{
+		public float initialDelay = 0;
+		public float ellipseDelay = 0;
+		public UserCharacterSO character = null;
+		public string text = "";
+	}
+
+	[System.Serializable]
+	public class DialogOption
+	{
+		public string previewText = "";
+		public string fullText = "";
+		public TreeNodeSO resultNode = null;
+	}
 }
