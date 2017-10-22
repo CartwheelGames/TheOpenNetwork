f5ac5296db0ce35392a5802701f089d6e53a1402
Michael Consoli <michael@cartwheelgames.com>
2017-10-22 00:06:25 -0400

    User character scriptable

---
+++ Assets/Source/Tree/UserCharacterSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserCharacterSO : ScriptableObject {
+	public Sprite avatar = null;
+	public string handle = "";
+    [TextArea]
+	public string bio;
+}
