74813b2db526790ae21aef9dd17c4dac5b5ed167
wvj1992 <wvj1992@gmail.com>
2017-10-23 12:32:21 -0400

    Rough Typewriting effect. Still working on it.

--- Assets/Source/CommentItem.cs
+++ Assets/Source/CommentItem.cs
@@ -5,17 +5,24 @@ using UnityEngine.UI;
 
 public class CommentItem : MonoBehaviour
 {
+	private float textDelay = 0.125f;
+
 	[SerializeField]
 	private Image avatarRenderer = null;
 	[SerializeField]
 	private Text contentLabel = null;
 	[SerializeField]
 	private Text timeStampLabel = null;
-	public void Setup(CharacterData character, string text, System.DateTime timestamp)
+
+
+    public void Setup (CharacterData character, string text, System.DateTime timestamp)
 	{
 		if (contentLabel != null && character && !string.IsNullOrEmpty(text))
 		{
 			contentLabel.text = "<color=darkblue>" + character.displayName + ":</color> " + text;
+			//contentLabel.text = "This is just a test.";
+			StartCoroutine (Typing());
+
 		}
 		if (avatarRenderer && character && character.avatar)
 		{
@@ -24,6 +31,13 @@ public class CommentItem : MonoBehaviour
 		if (timeStampLabel != null)
 		{
 			timeStampLabel.text = timestamp.ToShortTimeString();
+		}
+	}
+
+	IEnumerator Typing(){
+		foreach (char c in contentLabel.text) {
+			contentLabel.text += c;
+			yield return new WaitForSeconds (textDelay);
 		}
 	}
 }
